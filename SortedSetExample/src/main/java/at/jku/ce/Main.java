505297f9a5dac029470a513f384bce53e84680fd
ClemensDaCoder <madincubus@hotmail.com>
2012-12-04 15:11:54 +0100

    organised imports

--- SortedSetExample/src/main/java/at/jku/ce/Main.java
+++ SortedSetExample/src/main/java/at/jku/ce/Main.java
@@ -1,5 +1,11 @@
 package at.jku.ce;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
