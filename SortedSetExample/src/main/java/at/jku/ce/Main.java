24d7787ceebaeba827054fd417632dd76d59579c
ClemensDaCoder <madincubus@hotmail.com>
2012-12-06 19:56:39 +0100

    Added logged and created some objects for testing output

--- SortedSetExample/src/main/java/at/jku/ce/Main.java
+++ SortedSetExample/src/main/java/at/jku/ce/Main.java
@@ -2,16 +2,28 @@ package at.jku.ce;
 
 import java.util.HashSet;
 import java.util.Set;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		Set<DomainObject> objectSet = new HashSet<DomainObject>();
+		objectSet.add(new DomainObject("Helga"));
+		objectSet.add(new DomainObject("Walter"));
+		objectSet.add(new DomainObject("Hans"));
+		objectSet.add(new DomainObject("Paula"));
+		objectSet.add(new DomainObject("Claudia"));
+		
+		for (DomainObject domainObject : objectSet) {
+			LOGGER.debug(domainObject.id);
+		}
 	}
 
 }
