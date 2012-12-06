2c6c8f6281f27e197b5974c966112bbd84f72975
Manuel <manuel.hochreiter@atn.nu>
2012-12-06 19:43:27 +0100

    added constructor

--- SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
@@ -2,15 +2,8 @@ package at.jku.ce;
 
 public class DomainObject {
 
-	public String id;
-
 	public DomainObject() {
 		super();
-	}
-	
-	public DomainObject(String id) {
-		super();
-		this.id = id;
 	}
 
 }
