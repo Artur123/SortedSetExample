a1e311b10cf2efaa30b17cff2ea7ecf91f041755
Manuel <manuel.hochreiter@atn.nu>
2012-12-06 19:56:01 +0100

    added identifier

--- SortedSetExample/src/main/java/at/jku/ce/Main.java
+++ SortedSetExample/src/main/java/at/jku/ce/Main.java
@@ -8,10 +8,17 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
 
+	public String id;
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+	}
+	
+	public DomainObject(String id) {
+		super();
+		this.id = id;
 	}
 
 }
